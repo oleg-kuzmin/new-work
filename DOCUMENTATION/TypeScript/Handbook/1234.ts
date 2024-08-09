ef723b5e8e9f759beca1e9bc22319b74f04afc65
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-08-09 17:18:00 +0300

    md

--- DOCUMENTATION/TypeScript/Handbook/1234.ts
+++ DOCUMENTATION/TypeScript/Handbook/1234.ts
@@ -1,12 +1,3 @@
-function printName(obj: { first: string; last?: string }) {
-  // Error - might crash if 'obj.last' wasn't provided!
-  console.log(obj.last.toUpperCase());
-  // 'obj.last' is possibly 'undefined'.
-  if (obj.last !== undefined) {
-    // OK
-    console.log(obj.last.toUpperCase());
-  }
-
-  // A safe alternative using modern JavaScript syntax:
-  console.log(obj.last?.toUpperCase());
+function printId(id: number | string) {
+  console.log(id.toUpperCase());
 }
