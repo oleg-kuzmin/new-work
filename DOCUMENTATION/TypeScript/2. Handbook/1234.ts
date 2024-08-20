caa052761c5507827de2e21cd7653fdfdaac0310
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-08-20 09:23:43 +0300

    md

--- DOCUMENTATION/TypeScript/2. Handbook/1234.ts
+++ DOCUMENTATION/TypeScript/2. Handbook/1234.ts
@@ -1,10 +1,12 @@
-const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
-const underWater1: Fish[] = zoo.filter(isFish);
-// или, эквивалентно
-const underWater2: Fish[] = zoo.filter(isFish) as Fish[];
+interface Shape {
+  kind: 'circle' | 'square';
+  radius?: number;
+  sideLength?: number;
+}
 
-// Предикат возможно придется использовать еще раз для более сложных примеров
-const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
-  if (pet.name === 'sharkey') return false;
-  return isFish(pet);
-});
+function getArea(shape: Shape) {
+  if (shape.kind === 'circle') {
+    return Math.PI * shape.radius ** 2;
+    // 'shape.radius' is possibly 'undefined'.
+  }
+}
