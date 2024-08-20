01c9c72c96063855cd8cc4664f514c280b8c2c1e
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-08-20 09:09:10 +0300

    md

--- DOCUMENTATION/TypeScript/2. Handbook/1234.ts
+++ DOCUMENTATION/TypeScript/2. Handbook/1234.ts
@@ -1,7 +1,10 @@
-let x = Math.random() < 0.5 ? 10 : 'hello world!'; // let x: string | number
+const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
+const underWater1: Fish[] = zoo.filter(isFish);
+// или, эквивалентно
+const underWater2: Fish[] = zoo.filter(isFish) as Fish[];
 
-x = 1;
-console.log(x); // let x: number
-
-x = true; // Type 'boolean' is not assignable to type 'string | number'.
-console.log(x); // let x: string | number
+// Предикат возможно придется использовать еще раз для более сложных примеров
+const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
+  if (pet.name === 'sharkey') return false;
+  return isFish(pet);
+});
