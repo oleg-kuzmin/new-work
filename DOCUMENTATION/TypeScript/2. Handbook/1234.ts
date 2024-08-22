fcc384853ebe1e0722d36f058ae09f6774da84b2
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-08-22 09:43:15 +0300

    md

--- DOCUMENTATION/TypeScript/2. Handbook/1234.ts
+++ DOCUMENTATION/TypeScript/2. Handbook/1234.ts
@@ -8,10 +8,22 @@ interface Square {
   sideLength: number;
 }
 
-type Shape = Circle | Square;
+interface Triangle {
+  kind: 'triangle';
+  sideLength: number;
+}
+
+type Shape = Circle | Square | Triangle;
 
 function getArea(shape: Shape) {
-  return Math.PI * shape.radius ** 2;
-  // Property 'radius' does not exist on type 'Shape'.
-  // Property 'radius' does not exist on type 'Square'.
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+    case 'square':
+      return shape.sideLength ** 2;
+    default:
+      const _exhaustiveCheck: never = shape;
+      // Type 'Triangle' is not assignable to type 'never'.
+      return _exhaustiveCheck;
+  }
 }
