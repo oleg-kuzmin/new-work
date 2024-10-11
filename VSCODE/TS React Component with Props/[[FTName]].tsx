0d4747b633eee137176c444836b5e4812265681c
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-10-11 20:16:48 +0300

    md

---
+++ VSCODE/TS React Component with Props/[[FTName]].tsx
@@ -0,0 +1,15 @@
+import styles from './[[FTName]].module.scss';
+
+interface [[FTName]]Props {
+  className?: string;
+}
+
+export function [[FTName]]({ className }: Readonly<[[FTName]]Props>) {
+  const externalClass = className ? ` ${className}` : '';
+
+  return (
+    <div className={styles.[[FTName]] + externalClass}>
+      [[FTName]]
+    </div>
+  );
+}
--- VSCODE/myGlobalTemplates/TS React Component/[[FTName]].tsx
+++ VSCODE/myGlobalTemplates/TS React Component/[[FTName]].tsx
@@ -1,14 +1,8 @@
 import styles from './[[FTName]].module.scss';
 
-interface [[FTName]]Props {
-  className?: string;
-}
-
-export function [[FTName]]({ className }: Readonly<[[FTName]]Props>) {
-  const externalClass = className ? ` ${className}` : '';
-
+export function [[FTName]]() {
   return (
-    <div className={styles.[[FTName]] + externalClass}>
+    <div className={styles.[[FTName]]}>
       [[FTName]]
     </div>
   );
